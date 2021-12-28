029b4afb1fb9dc381527c3e7a6c38b864e2fa6f3
cuong <c.pham@odysseemobile.com>
2021-12-28 17:12:08 +0700

    init odoo connector app

---
+++ odooconnector/static/description/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Odyssee Odoo Connector',
+    'version':'14.0.0.0.0',
+    'description':'Odyssee Odoo Connector',
+    'author':'Odyssee Mobile',
+    'website':'https://odysseemobile.com/',
+    'license':'LGPL-3',
+    'images':['static/description/icon.png']
+}
