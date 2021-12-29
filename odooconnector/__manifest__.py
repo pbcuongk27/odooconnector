a56e900933701a661635cee2a389516b55e4d656
cuong <c.pham@odysseemobile.com>
2021-12-29 10:43:09 +0700

    Odoo Apps - Update

--- odooconnector/__manifest__.py
+++ odooconnector/__manifest__.py
@@ -5,5 +5,5 @@
     'author':'Odyssee Mobile',
     'website':'https://odysseemobile.com/',
     'license':'LGPL-3',
-    'images':['static/description/ods_description.png']
+    'images':['static/description/ods_description.png','static/description/odoo_introduce_screentshot.png']
 }
