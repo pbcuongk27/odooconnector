a85235da47e896dcfd9597b280474ef99f9e5260
cuong <c.pham@odysseemobile.com>
2021-12-29 09:45:34 +0700

    Odoo Apps - Add screenshots

--- odooconnector/__manifest__.py
+++ odooconnector/__manifest__.py
@@ -1,9 +1,9 @@
 {
     'name': 'Odyssee Odoo Connector',
-    'version':'14.0.0.0.0',
+    'version':'1.0.0.0.0',
     'description':'Odyssee Odoo Connector',
     'author':'Odyssee Mobile',
     'website':'https://odysseemobile.com/',
     'license':'LGPL-3',
-    'images':['static/description/icon.png']
+    'images':['static/description/icon.png','images/odoo_introduce_screentshot','images/odoo_auth_screenshot','images/odoo_settings_screenshot','images/odoo_categories_screenshot']
 }
